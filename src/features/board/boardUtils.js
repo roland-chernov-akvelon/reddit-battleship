c3a5fe7164189cf6a8e6c5a2873df53bffed6ac6
Roland Chernov <roland.chernov@akvelon.com>
2020-10-21 15:54:24 +0300

    Fix matrix build method

--- src/features/board/boardUtils.js
+++ src/features/board/boardUtils.js
@@ -2,7 +2,7 @@ import { CELL_EMPTY, CELL_SHIP, CELL_HIT, CELL_MISS } from "./boardCell.type";
 
 // TODO: validate input
 export function buildBoardMatrix(layout) {
-  const matrix = new Array(10).fill(new Array(10).fill(CELL_EMPTY));
+  const matrix = new Array(10).fill(null).map(() => new Array(10).fill(CELL_EMPTY));
   for (const ship of layout) {
     for (const pos of ship.positions) {
       const [x, y] = pos;
